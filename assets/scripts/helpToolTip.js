501fd1e4c87b68420d385b4031ba2593d0a4be9f
shyc2010@gmail.com <shyc2010@gmail.com@bdfd56bd-397d-8b1c-f164-87c4fe37fcdc>
2011-09-03 06:26:23 +0000

    +Added: PAC Import. +Added: Apply profile to all rules. #Fixed: Valid HTML5.
    git-svn-id: http://switchysharp.googlecode.com/svn/trunk@25 bdfd56bd-397d-8b1c-f164-87c4fe37fcdc

--- assets/scripts/helpToolTip.js
+++ assets/scripts/helpToolTip.js
@@ -25,13 +25,9 @@ HelpToolTip.onMouseOver = function onMouseOver(event) {
 	if (HelpToolTip.lastTipElement != undefined)
 		HelpToolTip.hide();
 	
-	var element = document.createElement('div');
-	element.id = 'helpTooltip';
-	element.innerHTML = event.toElement.innerHTML;
+	var element = document.getElementById("help_" + event.toElement.getAttribute("data-help"));
 	element.style.top = 0;
 	element.style.left = 0;
-	element.style.visibility = 'hidden';
-	document.body.appendChild(element);
 
 	var width = element.offsetWidth;
 	var height = element.offsetHeight;
@@ -68,17 +64,19 @@ HelpToolTip.show = function show() {
 
 HelpToolTip.hide = function hide() {
 	if (HelpToolTip.lastTipElement) { 
-		HelpToolTip.lastTipElement.parentNode.removeChild(HelpToolTip.lastTipElement);
+		//HelpToolTip.lastTipElement.parentNode.removeChild(HelpToolTip.lastTipElement);
+		HelpToolTip.lastTipElement.style.visibility = 'hidden';
 		HelpToolTip.lastTipElement = undefined;
 	}
 };
 
 HelpToolTip.enableTooltips = function enableTooltips() {
-	var helpElements = document.getElementsByClassName('help');
+	$("span[data-help]").click(HelpToolTip.show).mouseout(HelpToolTip.onMouseOut).mouseover(HelpToolTip.onMouseOver);
+	//var helpElements = document.getElementsByClassName('help');
 
-	for (var i = 0, helpElement; helpElement = helpElements[i]; i++) {
-		helpElement.onmouseover = HelpToolTip.onMouseOver;
-		helpElement.onmouseout = HelpToolTip.onMouseOut;
-		helpElement.onclick = HelpToolTip.show;
-	}
+	//for (var i = 0, helpElement; helpElement = helpElements[i]; i++) {
+	//	helpElement.onmouseover = HelpToolTip.onMouseOver;
+	//	helpElement.onmouseout = HelpToolTip.onMouseOut;
+	//	helpElement.onclick = HelpToolTip.show;
+	//}
 };
